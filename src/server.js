69a2150c98b7606b2280ffaada2cae2a9e93cf4e
Muhammad Nur Sidiq Sutrisno <muhammadnss.enterprise@gmail.com>
2022-07-29 10:16:58 +0700

    Implement dotenv + apply process env in server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
+'use strict'
 // import { Knex } from 'knex';
 const Hapi = require('@hapi/hapi');
+const path = require('path');
 const routes = require('./routes');
-const validate = require('./validate')
-// const auth = require('hapi-auth-jwt');
+const validate = require('./validate');
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env'
+const envPath = path.resolve(`${__dirname}/../${envFile}`); 
+const auth = require('hapi-auth-jwt');
 
+require('dotenv').config({ path: envPath});
+
+const { SERVER_HOST, SERVER_PORT} = process.env
 const internals = {};
 
 internals.init = async () => {
     const server = Hapi.server({
-         port: 5000,
-         host: 'localhost',
+        host: SERVER_HOST,
+        port: SERVER_PORT,
          routes: {
           cors: {
               origin: ['*'],
