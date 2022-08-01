e97712f35e399b00d7fac85eff9f7111a1b9f7f5
Muhammad Nur Sidiq Sutrisno <muhammadnss.enterprise@gmail.com>
2022-08-01 11:53:00 +0700

    Delete unused code on server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 'use strict'
-// import { Knex } from 'knex';
+
 const Hapi = require('@hapi/hapi');
 const path = require('path');
 const routes = require('./routes');
@@ -34,36 +34,9 @@ internals.init = async () => {
     });
 
     server.auth.default('jwt');
-
     server.route(routes);
     await server.start();
     console.log(`Server sudah dijalankan pada ${server.info.uri}`);
-
-    //===Authentication ngerjain nya pending dulu===
-
-    // server.register( require( 'hapi-auth-jwt' ), ( _err ) => {
-    //     server.auth.strategy( 'token', 'jwt', {
-    
-    //         key: 'ikSk9ksd0SSD3d0jdwDq20jdn8dmXdf1',
-    //         verifyOptions: {
-    //             algorithms: ['HS256'],
-    //         }
-    
-    //     });
-    
-    // });
-    // await server.register( 
-    //     plugin: require("hapi-auth-jwt")
-    // );
-
-    // server.auth.strategy('token', 'jwt', {
-    //     key: 'ikSk9ksd0SSD3d0jdwDq20jdn8dmXdf1',
-    //     verifyOptions: {
-    //         algorithm: 'HS256'
-    //     }
-    // })
-
-    console.log('Server sudah dijalankan pada %s', server.info.uri);
 };
 
 internals.init();
